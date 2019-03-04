0ac4ffe82125eb53e06216cd47152661ad402e8a
Onengiye Richard <klevamane@gmail.com>
2019-03-04 17:32:26 +0100

    add login and signup route link

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import './App.css';
 
 import Navbar from './components/layouts/Navbar';
 import Footer from './components/layouts/Footer';
 import Landing from './components/layouts/Landing';
+import Register from './components/authentication/Register';
+import Login from './components/authentication/Login';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-      <Navbar/>
-      <Landing/>
-      <Footer/>
-      </div>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Route exact path="/" component={Landing}/>
+          <div className="container">
+            <Route exact path="/register" component={Register}></Route>
+            <Route exact path="/login" component={Login}></Route>
+          </div>
+          <Footer />
+        </div>
+      </Router>
     );
   }
 }
