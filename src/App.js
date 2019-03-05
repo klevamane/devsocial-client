739e172d367b7b9c672836ae1438162e43b82b66
Richard Onengiye <31253707+klevamane@users.noreply.github.com>
2019-03-05 10:43:02 +0100

    Revert "# Add login and register react setup"

--- src/App.js
+++ src/App.js
@@ -1,28 +1,26 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-
+import logo from './logo.svg';
 import './App.css';
-
-import Navbar from './components/layouts/Navbar';
-import Footer from './components/layouts/Footer';
-import Landing from './components/layouts/Landing';
-import Register from './components/authentication/Register';
-import Login from './components/authentication/Login';
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="App">
-          <Navbar />
-          <Route exact path="/" component={Landing}/>
-          <div className="container">
-            <Route exact path="/register" component={Register}></Route>
-            <Route exact path="/login" component={Login}></Route>
-          </div>
-          <Footer />
-        </div>
-      </Router>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
     );
   }
 }
